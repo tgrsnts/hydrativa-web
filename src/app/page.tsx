4d3b7ad331c40ebd09b13141b6f05bede34626f8
tgrsnts <tegarsantoso72@gmail.com>
2024-11-03 21:52:25 +0700

    update tampilan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useState, useEffect,  } from "react";
+import { useState, useEffect, FormEvent } from "react";
 import { FcGoogle } from "react-icons/fc";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { FaEye, FaEyeSlash, FaShoppingCart, FaStar } from "react-icons/fa";
 import Footer from "@/components/Footer";
 import { Produk } from "@/lib/interfaces/Produk";
 import axios from "axios";
@@ -16,11 +16,6 @@ export default function Home() {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [products, setProducts] = useState<Produk[]>([]);
 
-  const router = useRouter();
-  const { login } = useAuth({
-    middleware: 'guest',
-    // redirectIfAuthenticated: '/dashboard',
-  });
 
   const submitHandler = async (event: FormEvent) => {
     event.preventDefault();
@@ -45,9 +40,6 @@ export default function Home() {
     username: '',
     password: ''
   });
-
-  const [error, setError] = useState(null);
-  const [products, setProducts] = useState<Produk[]>([]); // Deklarasikan state products
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -79,7 +71,7 @@ export default function Home() {
       try {
         const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/produk');
         if (!response.ok) throw new Error('Failed to fetch products');
-        
+
         const result = await response.json();
         setProducts(result.data);
       } catch (error) {
@@ -111,13 +103,15 @@ export default function Home() {
                 <a href="#hero">Home</a>
               </li>
               <li className="text-white border-b-2 border-primary md:mr-12 hover:border-white">
-                <a href="#tentang">Tentang Kami</a>
-              </li>
-              <li className="text-white border-b-2 border-primary md:mr-12 hover:border-white">
                 <a href="#our-products">Produk</a>
               </li>
+              <li className="text-white border-b-2 border-primary md:mr-12 hover:border-white">
+                <a href="#cart">
+                  <FaShoppingCart className="text-white" />
+                </a>
+              </li>
               <button
-                className="text-white border-2 md:mr-12 px-4 py-2 border-white cursor-pointer hover:bg-[#035700] hover:border-primary focus:bg-[#035700] focus:border-primary"
+                className="text-white border-2 md:mr-12 px-4 py-2 rounded-md border-white cursor-pointer hover:bg-primary hover:border-primary"
                 onClick={() => {
                   const modal = document.getElementById('modal_login') as HTMLDialogElement | null;
                   if (modal) {
@@ -127,9 +121,17 @@ export default function Home() {
               >
                 Masuk
               </button>
-              <li className="text-white border-2 border-white md:mr-12 px-4 py-2 hover:bg-[#035700] hover:border-primary focus:bg-[#035700] focus:border-primary">
-                <a href="dashboard">Dashboard</a>
-              </li>
+              <button
+                className="text-primary border-2 md:mr-12 px-4 py-2 rounded-md bg-white cursor-pointer hover:text-white hover:bg-primary hover:border-white"
+                onClick={() => {
+                  const modal = document.getElementById('modal_register') as HTMLDialogElement | null;
+                  if (modal) {
+                    modal.showModal();
+                  }
+                }}
+              >
+                Daftar
+              </button>
             </ul>
           </nav>
         </div>
@@ -184,12 +186,12 @@ export default function Home() {
                     placeholder="Masukkan password"
                     className="w-full p-2 rounded-md bg-gray-100 focus:outline-none focus:ring focus:ring-primary focus:border-primary"
                     required
-                    />
+                  />
                 </div>
                 <a
                   href=""
                   className="mt-1 text-sm text-primary hover:text-additional2 hover:underline hover:underline-offset-4"
-                  >
+                >
                   Lupa password?
                 </a>
               </div>
@@ -399,7 +401,7 @@ export default function Home() {
           </div>
       </div> */}
             {/* Card */}
-            <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 lg:justify-center">
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
               {products.length > 0 ? (
                 products.map((product) => (
                   <a
@@ -412,18 +414,15 @@ export default function Home() {
                       alt={product.nama} // Menggunakan nama produk sebagai alt
                       className="w-full object-cover mb-2 rounded-t-lg"
                     />
-                    <h3 className="text-sm lg:text-lg font-poppins font-semibold">
-                      {product.nama} {/* Menggunakan properti yang benar */}
-                    </h3>
-                    <div className="flex items-center justify-center gap-1">
-                      <i className="fa-solid fa-star text-yellow-400" />
-                      <div className="font-poppins text-gray-600">4.5</div>
-                    </div>
-                    <div className="font-poppins text-gray-700 mb-2">Rp {product.harga}</div>
-                    <div className="mt-auto">
-                      <button className="bg-white ring-2 ring-primary text-primary rounded-md px-4 py-1 font-poppins mb-4 hover:ring-2 hover:ring-primary hover:bg-primary hover:text-white">
-                        Beli
-                      </button>
+                    <div className="flex flex-col items-start p-4 pt-0">
+                      <p className="text-sm lg:text-lg font-poppins font-semibold">
+                        {product.nama} {/* Menggunakan properti yang benar */}
+                      </p>
+                      <div className="font-poppins text-gray-700">Rp {product.harga}</div>
+                      <div className="flex items-center justify-start gap-1">
+                        <FaStar className="text-yellow-400"></FaStar>
+                        <div className="font-poppins text-gray-600">4.5</div>
+                      </div>
                     </div>
                   </a>
                 ))
@@ -431,105 +430,95 @@ export default function Home() {
                 // <p>Loading products...</p> // Menangani kondisi ketika tidak ada produk
                 <>
                   <a
-                    href="detail.html"
+                    href={`/detail/1`} // Adjusted path as needed
                     className="flex flex-col w-full lg:w-full bg-white rounded-lg shadow-md transition-transform duration-300 transform hover:bg-gray-100 hover:scale-105"
+                    key="product1"
                   >
                     <img
                       src="/storage/produk/Group 185.png"
                       alt="Ayam Goreng"
                       className="w-full object-cover mb-2 rounded-t-lg"
                     />
-                    <h3 className="text-sm lg:text-lg font-poppins font-semibold">HydraTiva</h3>
-                    <div className="flex items-center justify-center gap-1">
-                      <i className="fa-solid fa-star text-yellow-400" />
-                      <div className="font-poppins text-gray-600">4.5</div>
-                    </div>
-                    <div className="font-poppins text-gray-700 mb-2">Rp 1.500.000</div>
-                    <div className="mt-auto">
-                      {/* <button
-                                      class="bg-primary rounded-md px-4 py-1 font-poppins text-white mb-4 hover:ring-2 hover:ring-primary hover:bg-white hover:text-primary">Beli</button> */}
-                      <button className="bg-white ring-2 ring-primary text-primary rounded-md px-4 py-1 font-poppins mb-4 hover:ring-2 hover:ring-primary hover:bg-primary hover:text-white">
-                        Beli
-                      </button>
+                    <div className="flex flex-col items-start p-4 pt-0">
+                      <p className="text-sm lg:text-lg font-poppins font-semibold">HydraTiva</p>
+                      <div className="font-poppins text-gray-700">Rp 1.500.000</div>
+                      <div className="flex items-center justify-start gap-1">
+                        <FaStar className="text-yellow-400"></FaStar>
+                        <div className="font-poppins text-gray-600">4.5</div>
+                      </div>
+                      
                     </div>
                   </a>
+
                   <a
-                    href="detail.html"
+                    href={`/detail/2`}
                     className="flex flex-col w-full lg:w-full bg-white rounded-lg shadow-md transition-transform duration-300 transform hover:bg-gray-100 hover:scale-105"
+                    key="product2"
                   >
                     <img
                       src="/storage/produk/Group 186.png"
                       alt="Es Jeruk"
                       className="w-full object-cover mb-2 rounded-t-lg"
                     />
-                    <h3 className="text-sm lg:text-lg font-poppins font-semibold">
-                      Daun Stevia Kering
-                    </h3>
-                    <div className="flex items-center justify-center gap-1">
-                      <i className="fa-solid fa-star text-yellow-400" />
-                      <div className="font-poppins text-gray-600">4.5</div>
-                    </div>
-                    <div className="font-poppins text-gray-700 mb-2">Rp 150.000</div>
-                    <div className="mt-auto">
-                      {/* <button
-                                      class="bg-primary rounded-md px-4 py-1 font-poppins text-white mb-4 hover:ring-2 hover:ring-primary hover:bg-white hover:text-primary">Beli</button> */}
-                      <button className="bg-white ring-2 ring-primary text-primary rounded-md px-4 py-1 font-poppins mb-4 hover:ring-2 hover:ring-primary hover:bg-primary hover:text-white">
-                        Beli
-                      </button>
+                    <div className="flex flex-col items-start p-4 pt-0">
+                      <p className="text-sm lg:text-lg font-poppins font-semibold">Daun Stevia Kering</p>
+                      <div className="font-poppins text-gray-700">Rp 150.000</div>
+                      <div className="flex items-center justify-start gap-1">
+                        <FaStar className="text-yellow-400"></FaStar>
+                        <div className="font-poppins text-gray-600">4.5</div>
+                      </div>                      
                     </div>
                   </a>
+
                   <a
-                    href="detail.html"
+                    href={`/detail/3`}
                     className="flex flex-col w-full lg:w-full bg-white rounded-lg shadow-md transition-transform duration-300 transform hover:bg-gray-100 hover:scale-105"
+                    key="product3"
                   >
                     <img
                       src="/storage/produk/teh stevia.jpeg"
                       alt="Nasi"
                       className="w-full object-cover mb-2 rounded-t-lg"
                     />
-                    <h3 className="text-sm lg:text-lg font-poppins font-semibold">
-                      Teh Stevia
-                    </h3>
-                    <div className="flex items-center justify-center gap-1">
-                      <i className="fa-solid fa-star text-yellow-400" />
-                      <div className="font-poppins text-gray-600">4.5</div>
-                    </div>
-                    <div className="font-poppins text-gray-700 mb-2">Rp 120.000</div>
-                    <div className="mt-auto">
-                      {/* <button
-                                      class="bg-primary rounded-md px-4 py-1 font-poppins text-white mb-4 hover:ring-2 hover:ring-primary hover:bg-white hover:text-primary">Beli</button> */}
-                      <button className="bg-white ring-2 ring-primary text-primary rounded-md px-4 py-1 font-poppins mb-4 hover:ring-2 hover:ring-primary hover:bg-primary hover:text-white">
-                        Beli
-                      </button>
+                    <div className="flex flex-col items-start p-4 pt-0">
+                      <p className="text-sm lg:text-lg font-poppins font-semibold">Teh Stevia</p>
+                      <div className="font-poppins text-gray-700">Rp 120.000</div>
+                      <div className="flex items-center justify-start gap-1">
+                        <FaStar className="text-yellow-400"></FaStar>
+                        <div className="font-poppins text-gray-600">4.5</div>
+                      </div>                      
                     </div>
                   </a>
+
                   <a
-                    href="detail.html"
+                    href={`/detail/4`}
                     className="flex flex-col w-full lg:w-full bg-white rounded-lg shadow-md transition-transform duration-300 transform hover:bg-gray-100 hover:scale-105"
+                    key="product4"
                   >
                     <img
                       src="/storage/produk/liquid stevia.jpeg"
                       alt="Es teh"
                       className="w-full object-cover mb-2 rounded-t-lg"
                     />
-                    <h3 className="text-sm lg:text-lg font-poppins font-semibold">
-                      Liquid Stevia
-                    </h3>
-                    <div className="flex items-center justify-center gap-1">
-                      <i className="fa-solid fa-star text-yellow-400" />
-                      <div className="font-poppins text-gray-600">4.5</div>
-                    </div>
-                    <div className="font-poppins text-gray-700 mb-2">Rp 96.000</div>
-                    <div className="mt-auto">
-                      {/* <button
-                                      class="bg-primary rounded-md px-4 py-1 font-poppins text-white mb-4 hover:ring-2 hover:ring-primary hover:bg-white hover:text-primary">Beli</button> */}
-                      <button className="bg-white ring-2 ring-primary text-primary rounded-md px-4 py-1 font-poppins mb-4 hover:ring-2 hover:ring-primary hover:bg-primary hover:text-white">
-                        Beli
-                      </button>
+                    <div className="flex flex-col items-start p-4 pt-0">
+                      <p className="text-sm lg:text-lg font-poppins font-semibold">Liquid Stevia</p>
+                      <div className="font-poppins text-gray-700">Rp 96.000</div>
+                      <div className="flex items-center justify-start gap-1">
+                        <FaStar className="text-yellow-400"></FaStar>
+                        <div className="font-poppins text-gray-600">4.5</div>
+                      </div>                      
                     </div>
                   </a>
                 </>
               )}
+            </div>
+            <div className="flex justify-end">
+              <a
+                href="/dashboard"
+                className="font-poppins text-primary border-2 md:mr-12 px-4 py-2 rounded-md bg-white cursor-pointer hover:text-white hover:bg-primary hover:border-white"
+              >
+                Lihat Selengkapnya
+              </a>
             </div>
           </div>
         </section>
