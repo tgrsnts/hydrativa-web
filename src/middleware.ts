6e014b5e398dcea8b341869857e70d6576801fb5
tgrsnts <tegarsantoso72@gmail.com>
2024-11-04 08:59:00 +0700

    delete verify

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 // middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { verify } from 'jsonwebtoken';
 
 export async function middleware(request: NextRequest) {
     // Ambil token dari cookies
