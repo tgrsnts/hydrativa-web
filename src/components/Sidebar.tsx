12604d65847956b09e5b8884ae71d70eb910a016
tgrsnts <tegarsantoso72@gmail.com>
2024-11-04 09:45:18 +0700

    ubah a ke link

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
 
@@ -30,14 +31,14 @@ export default function Sidebar() {
 
     return (
         <nav className="flex flex-col gap-1 pr-12 font-sans text-base font-normal text-blue-gray-700">
-            <a href="/" className="flex items-center whitespace-nowrap text-2xl">
+            <Link href="/" className="flex items-center whitespace-nowrap text-2xl">
                 <img
                     className="ml-8 w-32 lg:w-24"
                     src="image/logo-hydrativa-putih.png"
                     alt=""
                 />
-            </a>
-            <a
+            </Link>
+            <Link
                 href="/akun"
                 className={`mt-8 ${pathname == '/akun' ? 'text-primary bg-white' : 'text-white'} font-poppins font-semibold flex items-center w-full py-4 pl-16 leading-tight transition-all rounded-r-lg outline-none text-start hover:bg-white hover:text-primary focus:bg-white focus:text-primary active:bg-white active:text-primary`}
             >
@@ -45,8 +46,8 @@ export default function Sidebar() {
                     <i className="fa-solid fa-user" />
                 </div>
                 Profil
-            </a>
-            <a
+            </Link>
+            <Link
                 href="/alamat"
                 className={`${pathname == '/alamat' ? 'text-primary bg-white' : 'text-white'} font-poppins font-semibold flex items-center w-full py-4 pl-16 leading-tight transition-all rounded-r-lg outline-none text-start hover:bg-white hover:text-primary focus:bg-white focus:text-primary active:bg-white active:text-primary`}
             >
@@ -54,8 +55,8 @@ export default function Sidebar() {
                     <i className="fa-solid fa-location-dot" />
                 </div>
                 Alamat
-            </a>
-            <a
+            </Link>
+            <Link
                 href="/pembelian"
                 className={`${pathname == '/pembelian' ? 'text-primary bg-white' : 'text-white'} font-poppins font-semibold flex items-center w-full py-4 pl-16 leading-tight transition-all rounded-r-lg outline-none text-start hover:bg-white hover:text-primary focus:bg-white focus:text-primary active:bg-white active:text-primary`}
             >
@@ -63,7 +64,7 @@ export default function Sidebar() {
                     <i className="fa-solid fa-bag-shopping" />
                 </div>
                 Pembelian
-            </a>
+            </Link>
             <div
                 role='button'
                 // onClick={onClickLogout}
