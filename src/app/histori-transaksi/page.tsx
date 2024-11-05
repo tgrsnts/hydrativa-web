ef74c615fbcb0cac1e5f98b906566e9dd21887fd
tgrsnts <tegarsantoso72@gmail.com>
2024-11-05 16:06:08 +0700

    update tampilan

--- src/app/histori-transaksi/page.tsx
+++ src/app/histori-transaksi/page.tsx
@@ -19,33 +19,21 @@ export default function Akun() {
                         id="dashboard"
                         className="min-h-screen font-poppins w-full flex gap-2 flex-col mt-2 pt-10 px-4 pb-20 bg-background"
                     >
-                        <div className="flex gap-2 rounded-lg bg-white w-full py-4">
-                            <div className="px-4 py-2">Status</div>
-                            <div className="px-4 py-2 text-[#047D00] rounded-lg border-2 border-[#047D00] bg-green-50 hover:bg-green-50 hover:text-[#047D00] hover:border-[#047D00]">
-                                Semua
-                            </div>
-                            <div className="px-4 py-2 rounded-lg border-2 border-gray-300 hover:bg-green-50 hover:text-[#047D00] hover:border-[#047D00]">
-                                Belum Bayar
-                            </div>
-                            <div className="px-4 py-2 rounded-lg border-2 border-gray-300 hover:bg-green-50 hover:text-[#047D00] hover:border-[#047D00]">
-                                Sedang Dikemas
-                            </div>
-                            <div className="px-4 py-2 rounded-lg border-2 border-gray-300 hover:bg-green-50 hover:text-[#047D00] hover:border-[#047D00]">
-                                Dikirim
-                            </div>
-                            <div className="px-4 py-2 rounded-lg border-2 border-gray-300 hover:bg-green-50 hover:text-[#047D00] hover:border-[#047D00]">
-                                Selesai
-                            </div>
-                            <div className="px-4 py-2 rounded-lg border-2 border-gray-300 hover:bg-green-50 hover:text-[#047D00] hover:border-[#047D00]">
-                                Dibatalkan
+                        <div className="flex flex-col gap-2 rounded-lg bg-white w-full py-4">
+                            <div className="px-4 py-2">
+                                <p className='font-semibold'>Histori Transaksi Saya</p>
+                                <p>Melihat daftar transaksi yang anda lakukan.</p>
                             </div>
                         </div>
                         <div className="flex flex-col gap-2">
                             <div className="flex gap-4 flex-col bg-white w-full p-4 rounded-lg">
+                                <div className="flex justify-end w-full gap-5">
+                                    <p>Status : Dalam Perjalanan</p>
+                                </div>
                                 <div className="flex w-full gap-5">
-                                    <img className="w-20" src="image/product/Artboard 25.png" alt="" />
+                                    <img className="w-20 rounded-lg" src="/storage/produk/teh stevia.jpeg" alt="" />
                                     <div className="flex flex-col gap- w-full">
-                                        <div>Jagung</div>
+                                        <div>Teh Daun Stevia</div>
                                         <div className="flex justify-between">
                                             <div>x1</div>
                                             <div>Rp. 4.000</div>
@@ -53,9 +41,49 @@ export default function Akun() {
                                     </div>
                                 </div>
                                 <div className="flex w-full gap-5">
-                                    <img className="w-20" src="image/product/Artboard 32.png" alt="" />
+                                    <img className="w-20 rounded-lg" src="/storage/produk/liquid stevia.jpeg" alt="" />
                                     <div className="flex flex-col gap- w-full">
-                                        <div>Brokoli</div>
+                                        <div>Liquid Stevia</div>
+                                        <div className="flex justify-between">
+                                            <div>x1</div>
+                                            <div>Rp. 4.000</div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <div className="flex items-center gap-2">
+                                        <a
+                                            href="checkout.html"
+                                            className="bg-primary font-poppins rounded-lg px-4 py-2 border-2 border-primary text-white text-center w-full hover:bg-white hover:text-primary"
+                                        >
+                                            Pesanan telah diterima
+                                        </a>
+                                    </div>
+                                    <div className="flex justify-end gap-4">
+                                        <div>Total Pesanan:</div>
+                                        <div className="text-primary">Rp. 8.000</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex gap-4 flex-col bg-white w-full p-4 rounded-lg">
+                                <div className="flex justify-end w-full gap-5">
+                                    <p>Status : Selesai</p>
+                                </div>
+                                <div className="flex w-full gap-5">
+                                    <img className="w-20 rounded-lg" src="/storage/produk/teh stevia.jpeg" alt="" />
+                                    <div className="flex flex-col gap- w-full">
+                                        <div>Teh Daun Stevia</div>
+                                        <div className="flex justify-between">
+                                            <div>x1</div>
+                                            <div>Rp. 4.000</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex w-full gap-5">
+                                    <img className="w-20 rounded-lg" src="/storage/produk/liquid stevia.jpeg" alt="" />
+                                    <div className="flex flex-col gap- w-full">
+                                        <div>Liquid Stevia</div>
                                         <div className="flex justify-between">
                                             <div>x1</div>
                                             <div>Rp. 4.000</div>
@@ -64,8 +92,12 @@ export default function Akun() {
                                 </div>
                                 <div className="flex justify-between">
                                     <div className="flex items-center gap-2">
-                                        <i className="fa-solid fa-truck" />
-                                        <div>Pesanan telah sampai</div>
+                                        <a
+                                            href="checkout.html"
+                                            className="bg-primary font-poppins rounded-lg px-4 py-2 border-2 border-primary text-white text-center w-full hover:bg-white hover:text-primary"
+                                        >
+                                            Review
+                                        </a>
                                     </div>
                                     <div className="flex justify-end gap-4">
                                         <div>Total Pesanan:</div>
@@ -77,7 +109,7 @@ export default function Akun() {
                                 <div className="flex w-full gap-5">
                                     <img className="w-20" src="image/product/Artboard 25.png" alt="" />
                                     <div className="flex flex-col gap- w-full">
-                                        <div>Jagung</div>
+                                        <div>Teh Daun Stevia</div>
                                         <div className="flex justify-between">
                                             <div>x1</div>
                                             <div>Rp. 4.000</div>
@@ -87,7 +119,7 @@ export default function Akun() {
                                 <div className="flex w-full gap-5">
                                     <img className="w-20" src="image/product/Artboard 32.png" alt="" />
                                     <div className="flex flex-col gap- w-full">
-                                        <div>Brokoli</div>
+                                        <div>Liquid Stevia</div>
                                         <div className="flex justify-between">
                                             <div>x1</div>
                                             <div>Rp. 4.000</div>
