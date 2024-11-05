7620458cc2fca1f0181dce3f8e920006d60e072e
tgrsnts <tegarsantoso72@gmail.com>
2024-11-05 14:24:40 +0700

    hapus search

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,31 +6,11 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <header className="font-poppins shadow fixed top-0 w-full z-10 h-20 bg-white">
-      <div className="bg-white flex items-center h-full flex-wrap gap-5 overflow-hidden px-4 py-4 md:px-36 md:mx-auto md:flex-wrap md:items-center">
+    <header className="font-poppins shadow fixed top-0 w-full z-10 h-20 bg-primary">
+      <div className="bg-primary flex items-center h-full flex-wrap gap-5 overflow-hidden px-4 py-4 md:px-36 md:mx-auto md:flex-wrap md:items-center">
         <Link href="/">
-          <img className="h-8" src="/image/logo-hydrativa-kecil.png" alt="" />
+          <img className="h-8" src="/image/logo-hydrativa-putih.png" alt="" />
         </Link>
-        <div className="flex ml-auto h-full lg:ml-0 flex-row w-max lg:w-96">
-          <form className="w-full mx-auto">
-            {/* <label for="default-search"
-                  class="mb-2 text-sm font-medium text-gray-900 sr-only :text-white">Search
-              </label> */}
-            <div className="relative h-full">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <IoIosSearch />
-              </div>
-              <input
-                type="search"
-                id="default-search"
-                className="block w-full h-full px-2 py-2 ps-8 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:border-[#047D00]"
-                placeholder="Cari di HydraTiva"
-              />
-              {/* <button type="submit"
-                      class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 :bg-blue-600 :hover:bg-blue-700 :focus:ring-blue-800">Search</button> */}
-            </div>
-          </form>
-        </div>
         <div className="hidden lg:flex items-center ml-auto gap-3 h-full">
           <div className="flex">
             {/* <button type="button" onclick="my_modal_1.showModal()"
