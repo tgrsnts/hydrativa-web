7ecd1895850bf6519a498bd6d06b1cb52fb5dbb2
tgrsnts <tegarsantoso72@gmail.com>
2024-11-13 06:28:13 +0700

    update interface

--- src/lib/interfaces/Transaksi.ts
+++ src/lib/interfaces/Transaksi.ts
@@ -2,12 +2,20 @@ import { Alamat } from "./Alamat";
 import { Produk } from "./Produk";
 
 interface Transaksi {
-    transaksi_id: number;
-    status: string;
-    total_harga: number;
-    pembeli: string;
-    alamat: Alamat;
-    produk: Produk[];
-  }
+  transaksi_id: number;
+  status: string;
+  total_harga: number;
+  pembeli: string;
+  alamat: Alamat;
+  produk: Array<{
+    transaksi_item_id: number;
+    produk_id: number;
+    nama_produk: string;
+    israted: number;
+    harga: number;
+    quantity: number;
+    gambar: string;    
+  }>;
+}
 
 export default Transaksi;
