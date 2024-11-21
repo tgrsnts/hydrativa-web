1b597c63be57573ad61a344b6a043828fe174820
tgrsnts <tegarsantoso72@gmail.com>
2024-11-21 09:37:10 +0700

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,29 +2,55 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  // Ambil token atau data pengguna dari cookies atau header
-  const token = req.cookies.get('token'); // Misalnya token disimpan dalam cookies
+  // Ambil token dan role dari cookies
+  const token = req.cookies.get('token');
+  const roleCookie = req.cookies.get('role');
+  const role = roleCookie ? roleCookie.value : undefined;
 
+  // Ambil path yang diminta
+  const urlPath = req.nextUrl.pathname;
+
+  // Jika path adalah /dashboard, izinkan akses tanpa validasi
+  if (urlPath === '/dashboard') {
+    return NextResponse.next();
+  }
+
+  // Jika tidak ada token, redirect ke halaman login
   if (!token) {
-    // Jika tidak ada token, redirect ke halaman login
     return NextResponse.redirect(new URL('/', req.url));
   }
 
-  // Misalnya, ambil role dari token atau data yang ada
-  const roleCookie = req.cookies.get('role'); // This will be of type RequestCookie | undefined
-  
-  // Check if the cookie exists and extract the role as a string
-  const role = roleCookie ? roleCookie.value : undefined;
-
-  // Cek role pengguna dan tentukan akses
-  if (role == '1') {
-    return NextResponse.next(); // Izinkan akses ke halaman
-  } else {
-    // Jika role bukan admin, redirect ke halaman tertentu
-    return NextResponse.redirect(new URL('/dashboard', req.url));
+  // Logika akses berdasarkan role dan path
+  if (urlPath.startsWith('/admin')) {
+    // Hanya role 1 yang dapat mengakses /admin
+    if (role !== '1') {
+      return NextResponse.redirect(new URL('/dashboard', req.url));
+    }
+  } else if (
+    urlPath.startsWith('/akun') ||
+    urlPath.startsWith('/alamat') ||
+    urlPath.startsWith('/checkout') ||
+    urlPath.startsWith('/histori-transaksi') ||
+    urlPath.startsWith('/keranjang')
+  ) {
+    // Hanya role 2 yang dapat mengakses rute ini
+    if (role !== '2') {
+      return NextResponse.redirect(new URL('/dashboard', req.url));
+    }
   }
+
+  // Jika tidak ada aturan yang dilanggar, izinkan akses
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/admin/:path*', '/akun', '/alamat', '/checkout', '/histori-transaksi', '/keranjang'], // Tentukan halaman yang akan dicek aksesnya (misal: /admin)
+  matcher: [
+    '/admin/:path*',
+    '/akun',
+    '/alamat',
+    '/checkout',
+    '/histori-transaksi',
+    '/keranjang',
+    '/dashboard',
+  ], // Tentukan halaman yang akan dicek aksesnya
 };
